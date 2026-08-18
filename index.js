aeecb2bc95d3f276e8c18a0ae8d619abedad95be
Amit Patel <redblobgames@gmail.com>
2026-08-18 13:48:17 -0700

    Reddit r/roguelikedev summer event 2026
    I'm following the 2020 Roguelike Tutorial for Python.
    https://rogueliketutorials.com/tutorials/tcod/v2/part-0/
    
    I'm using Javascript, with Typescript to show possible type errors in
    the IDE. This gives me soft type checking, completions, some type
    error detection, but doesn't require that everything be type
    annotated. And it doesn't require a build step, which is important to
    me.
    
    I'm using ROT.js, so I've included that library in rotjs/ . It's open
    source, BSD-3-Clause license (see NOTICES).

---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+ * From https://www.redblobgames.com/x/2634-roguelike-dev/
+ * Copyright 2026 Red Blob Games <redblobgames@gmail.com>
+ * @license Apache-2.0 <https://www.apache.org/licenses/LICENSE-2.0.html>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Display } from "./third-party/rotjs/index.js";
+
+console.log("Hello, world");
