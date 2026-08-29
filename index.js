16cd904ffbb3b6966682a300fb1f2f385465a090
Amit Patel <redblobgames@gmail.com>
2026-08-29 13:10:07 -0700

    Part 7: Add a message log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 import { RNG, Map as RotMap, FOV } from "./third-party/rotjs/index.js";
 import { Table } from "./table.js";
-import { screenSize, drawWorld, drawTable, setupInputHandlers } from "./interface.js";
+import { print, screenSize, drawWorld, drawTable, drawMessages, setupInputHandlers } from "./interface.js";
 
 RNG.setSeed(1234);
 const randint = RNG.getUniformInt.bind(RNG);
@@ -159,13 +159,13 @@ function handleCombat(attacker, defender) {
     } else {
         message += ` but does no damage.`;
     }
-    console.log(message);
+    print(message);
     if (defender.hp === 0) handleDeath(defender);
 }
 
 function handleDeath(actor) {
     let message = actor === world.player? `You died!` : `${actor.type} is dead!`;
-    console.log(message);
+    print(message);
     actor.type = 'corpse';
     actor.ai = undefined;
 }
@@ -190,7 +190,7 @@ function handlePlayerAction(action) {
                 handleCombat(world.player, blockingEntity);
                 return true;
             } else if (blockingEntity) {
-                console.log(`You cannot walk through ${blockingEntity.type}.`);
+                print `You cannot walk through ${blockingEntity}.`;
                 return false;
             } else {
                 world.player.location = {type: 'map', x: newX, y: newY};
@@ -308,6 +308,7 @@ function drawAll() {
     });
     drawWorld(world);
     drawTable("#world-entities", world.entities);
+    drawMessages();
 }
 
 setupInputHandlers(handleKeyDown);
