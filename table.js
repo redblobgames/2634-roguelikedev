c75d4aab0100341a1d5275cebe2f49bf2a7eb5f2
Amit Patel <redblobgames@gmail.com>
2026-08-31 10:53:58 -0700

    Extras: Make the table editable

--- table.js
+++ table.js
@@ -29,6 +29,11 @@ export class Table {
         location: {unique: false, static: false}, // used with entities, where multiple can be in a location
     };
 
+    /**
+     * @param{string} name
+     * @param{Array<string>} columns
+     * @param{Record<string, object>} prototypes
+     */
     constructor(name, columns, prototypes) {
         this.name = name;
         this.prototypes = prototypes;
