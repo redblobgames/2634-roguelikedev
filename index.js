b85cecf15de1eaa0c65adcf389ba80083837284c
Amit Patel <redblobgames@gmail.com>
2026-09-05 19:50:34 -0700

    Extras: Add ability for entities to block the view
    Even though this isn't a feature from the Python tutorial, I think it's
    a good demonstration of the table editing feature. Toggle the blocksView
    column on any of the prototypes to see the effect. By default Trolls
    block the view.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ let components = {
         return {holdable: false, blocksMovement: true, ...this.fighter(hp, defense, attack)};
     },
     holdable() {
-        return {blocksMovement: false, holdable: true};
+        return {blocksView: false, blocksMovement: false, holdable: true};
     },
 };
 
@@ -40,13 +40,13 @@ world = {
     entities: new Table('Entities', ['location', 'hp', 'inventory', 'ai'],
         {
             player: {
-                shape: "@", fg: "hsl(60 100% 50%)", renderOrder: 1, blocksMovement: false, holdable: false,
+                shape: "@", fg: "hsl(60 100% 50%)", renderOrder: 1, blocksView: false, blocksMovement: false, holdable: false,
                 ...components.fighter(30, 2, 5),
                 get inventory() { return world && world.entities.findAll({location: {type: 'held', by: world.player.id}}); },
             },
-            corpse: {shape: "%", fg: "hsl(  0 20% 50%)", renderOrder: 9, blocksMovement: false, holdable: false},
-            troll:  {shape: "T", fg: "hsl(120 60% 50%)", renderOrder: 2, ...components.enemy(10, 0, 3)},
-            orc:    {shape: "o", fg: "hsl(100 30% 50%)", renderOrder: 2, ...components.enemy(16, 1, 4)},
+            corpse: {shape: "%", fg: "hsl(  0 20% 50%)", renderOrder: 9, blocksView: false, blocksMovement: false, holdable: false},
+            troll:  {shape: "T", fg: "hsl(120 60% 50%)", renderOrder: 2, blocksView: true, ...components.enemy(10, 0, 3)},
+            orc:    {shape: "o", fg: "hsl(100 30% 50%)", renderOrder: 2, blocksView: false, ...components.enemy(16, 1, 4)},
             health_potion: {shape: "!", fg: "rgb(127 0 255)", renderOrder: 3, ...components.holdable(), consumable: {type: 'heal', amount: 4}},
             lightning_potion: {shape: "~", fg: "rgb(255 255 0)", renderOrder: 3, ...components.holdable(), consumable: {type: 'lightning', damage: 20, range: 5}},
             confusion_scroll: {shape: "~", fg: "rgb(207 63 255)", renderOrder: 3, ...components.holdable(), consumable: {type: 'confusion', turns: 10}},
@@ -60,7 +60,10 @@ world = {
         }
     ),
     player: null,
-    fov: new FOV.PreciseShadowcasting((x, y) => world.tiles.findAny({position: {x, y}})?.transparent),
+    fov: new FOV.PreciseShadowcasting((x, y) =>
+        world.tiles.findAny({position: {x, y}})?.transparent
+            && !world.entities.findFirst({location: {type: 'map', x, y}, blocksView: true})
+    ),
     nextTurn() {
         // let enemies move
         for (let entity of world.entities.findAll({ai: Table.ANY})) {
